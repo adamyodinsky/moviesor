1aa999bca2b8edaef1dfd515e039a439dc31293c
Adam Yodinsky <adam@zooz.com>
2019-10-07 21:29:19 +0300

    clean start

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,48 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import  classNames from 'classnames';
 import logo from './logo.svg';
-// import './styles.scss';
-import './test.scss'
-import MochooButton from './components/MochooButton/MochooeButton'
 
-class App extends Component {
-    state = {
-      animated: false,
-    };
-
-
-  clicked = (event) => {
-    this.setState({
-      animated: true,
-    });
-
-    setTimeout(() => {
-      this.setState({
-        animated: false,
-      });
-      }, 1300
-    );
-  };
-
-  render() {
-
-
+ const App = () => {
     return (
         <div>
-          <a className={classNames('btn', {
-            'btn--clicked': this.state.animated
-          })} onClick={this.clicked}>MoChooe</a>
-          {
-            ['blue', 'orange', 'green', 'white'].map((color) => (
-                <span className={classNames('color', `color--${color}`, {
-                  expanded: this.state.animated
-                })} data-value="1" />
-            ))
-          }
+          <p>React!</p>
         </div>
-
     )
-  }
-}
+ };
 
 export default App;
